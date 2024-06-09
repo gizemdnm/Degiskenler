60fb60dcfe9cbdeb7945cf392f01414950301102
gizem.donum <gizem.donum@301-01>
2024-06-09 13:52:39 +0300

    Add project files.

---
+++ DegiskenOrnekleri/Program.cs
@@ -0,0 +1,76 @@
+﻿//1.Tam Sayı Değişkeni Kullanımı
+
+/*int age = 18; //age=>değişken, 18=>değer
+Console.WriteLine("Alperen'in Yaşı: " + age);*/
+
+//2.Ondalıklı Sayı Değişkeni Kullanımı
+
+/*double price = 99.99;
+Console.WriteLine("Ürünün Fiyatı: " + price);*/
+
+//3.Karakter(char) Değişkeni Kullanımı
+
+/*char harf = 'A';
+Console.WriteLine("Harf: " + harf);*/
+
+
+//Mantıksal (boolean) Değişken Kullanımı
+
+/*bool isSunny = false;
+Console.WriteLine("Hava güneşli mi:" + isSunny);
+Console.ReadLine();*/
+
+//4.Metinsel(string) Değişken Kullanımı
+
+/*string name = "Berk";
+Console.WriteLine("Adı: " + name);
+Console.ReadLine();*/
+
+
+//5.İki Tam Sayının Toplamını Bulma
+
+/*int sayi = 5;
+int sayi2 = 6;
+int toplam = sayi + sayi2;
+Console.WriteLine("Toplam: " + toplam);*/
+
+
+//6.Ondalıklı sayı değişkenlerinin çarpımını bulma
+/*double x = 5.2;
+double y = 2.5;
+double product = x * y;
+Console.WriteLine("x ve y'nin çarpımı = " + product);
+Console.ReadLine();*/
+
+//7.Karakter Değişkenleri ile Metin ya da Karakter Birleştirme
+
+/*1.İki değişkeni birleştirme
+/*char İlkHarf = 'h';
+char ikinciHarf = 'i';
+string a = İlkHarf + ikinciHarf + "Cem";
+Console.WriteLine(a);
+Console.ReadLine();*/
+
+/*2.Unicode olan iki değişkenin string.concat yöntemiyle metinsel ifadeye dönüştürme
+char İlkHarf = 'h';
+char ikinciHarf = 'i';
+string a = string.Concat(İlkHarf,ikinciHarf, " Cem");
+Console.WriteLine(a);
+Console.ReadLine();*/
+
+//8.Mantıksal(boolean) Değişkenleri Kullanarak Koşul İfadesi Oluşturma
+
+/*bool isRaining = true;
+if (isRaining)
+{
+    Console.WriteLine("Şemsiye almanı tavsiye ederim!");
+}
+else
+{
+    Console.WriteLine("Hava güneşli.Dışarı çıkabilirsin");
+}
+Console.ReadLine();*/
+
+
+
+
---
+++ KullanıcıdanDegerAlma1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KullanıcıdanDegerAlma1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fiyat Girin: ");
+            double fiyat = double.Parse(Console.ReadLine());
+
+            Console.Write("Kar oranını girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            double sonuc = fiyat + ((fiyat * sayi) / 100);
+            Console.WriteLine("Satış Fiyatı: " + sonuc);
+        }
+    }
+}
